1e1a48633bfdfe6394f017d209e2c899a9f7943a
Max <122988460+MaximusMixik@users.noreply.github.com>
2024-11-03 14:59:16 +0200

    Add files via upload

--- hnm/js/app.js
+++ hnm/js/app.js
@@ -446,6 +446,11 @@
                 }
             }));
         }
+        function functions_FLS(message) {
+            setTimeout((() => {
+                if (window.FLS) console.log(message);
+            }), 0);
+        }
         function uniqArray(array) {
             return array.filter((function(item, index, self) {
                 return self.indexOf(item) === index;
@@ -489,6 +494,61 @@
                 }
             }
         }
+        class MousePRLX {
+            constructor(props, data = null) {
+                let defaultConfig = {
+                    init: true,
+                    logging: true
+                };
+                this.config = Object.assign(defaultConfig, props);
+                if (this.config.init) {
+                    const paralaxMouse = document.querySelectorAll("[data-prlx-mouse]");
+                    if (paralaxMouse.length) {
+                        this.paralaxMouseInit(paralaxMouse);
+                        this.setLogging(`Прокинувся, стежу за об'єктами: (${paralaxMouse.length})`);
+                    } else this.setLogging("Немає жодного обєкта. Сплю...");
+                }
+            }
+            paralaxMouseInit(paralaxMouse) {
+                paralaxMouse.forEach((el => {
+                    const paralaxMouseWrapper = el.closest("[data-prlx-mouse-wrapper]");
+                    const paramСoefficientX = el.dataset.prlxCx ? +el.dataset.prlxCx : 100;
+                    const paramСoefficientY = el.dataset.prlxCy ? +el.dataset.prlxCy : 100;
+                    const directionX = el.hasAttribute("data-prlx-dxr") ? -1 : 1;
+                    const directionY = el.hasAttribute("data-prlx-dyr") ? -1 : 1;
+                    const paramAnimation = el.dataset.prlxA ? +el.dataset.prlxA : 50;
+                    let positionX = 0, positionY = 0;
+                    let coordXprocent = 0, coordYprocent = 0;
+                    setMouseParallaxStyle();
+                    if (paralaxMouseWrapper) mouseMoveParalax(paralaxMouseWrapper); else mouseMoveParalax();
+                    function setMouseParallaxStyle() {
+                        const distX = coordXprocent - positionX;
+                        const distY = coordYprocent - positionY;
+                        positionX += distX * paramAnimation / 1e3;
+                        positionY += distY * paramAnimation / 1e3;
+                        el.style.cssText = `transform: translate3D(${directionX * positionX / (paramСoefficientX / 10)}%,${directionY * positionY / (paramСoefficientY / 10)}%,0) rotate(0.02deg);`;
+                        requestAnimationFrame(setMouseParallaxStyle);
+                    }
+                    function mouseMoveParalax(wrapper = window) {
+                        wrapper.addEventListener("mousemove", (function(e) {
+                            const offsetTop = el.getBoundingClientRect().top + window.scrollY;
+                            if (offsetTop >= window.scrollY || offsetTop + el.offsetHeight >= window.scrollY) {
+                                const parallaxWidth = window.innerWidth;
+                                const parallaxHeight = window.innerHeight;
+                                const coordX = e.clientX - parallaxWidth / 2;
+                                const coordY = e.clientY - parallaxHeight / 2;
+                                coordXprocent = coordX / parallaxWidth * 100;
+                                coordYprocent = coordY / parallaxHeight * 100;
+                            }
+                        }));
+                    }
+                }));
+            }
+            setLogging(message) {
+                this.config.logging ? functions_FLS(`[PRLX Mouse]: ${message}`) : null;
+            }
+        }
+        modules_flsModules.mousePrlx = new MousePRLX({});
         function ssr_window_esm_isObject(obj) {
             return obj !== null && typeof obj === "object" && "constructor" in obj && obj.constructor === Object;
         }
